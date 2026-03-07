434f43dc86fce72c640b26db9d55d4815cd2ad90
Jiyajoe <jiyajoe75@gmail.com>
2026-03-07 23:47:54 +0530

    update backend config

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',       # ← Change to your MySQL username
-    'password': 'jania',       # ← Change to your MySQL password
+    'host': ' 192.168.56.1',
+    'user': 'rentflow',       # ← Change to your MySQL username
+    'password': 'password',       # ← Change to your MySQL password
     'database': 'rentflow_db',
     'charset': 'utf8mb4'
 }
